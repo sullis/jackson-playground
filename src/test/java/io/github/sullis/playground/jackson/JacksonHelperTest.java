2402af5e313a9ccc848b9c6d353d91a56eba6c40
sullis <github@seansullivan.com>
2025-11-03 19:30:36 -0800

    implement unit test

--- src/test/java/io/github/sullis/playground/jackson/JacksonHelperTest.java
+++ src/test/java/io/github/sullis/playground/jackson/JacksonHelperTest.java
@@ -2,11 +2,23 @@ package io.github.sullis.playground.jackson;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JacksonHelperTest {
+
   @Test
-  void happyPath() {
-      
+  void happyPath() throws Exception {
+    var helper = new JacksonHelper();
+    var bean = new SimpleBean();
+    var json = helper.toJsonString(bean);
+    var bean2 = helper.fromJsonString(json, SimpleBean.class);
+
+    assertThat(bean2.count).isEqualTo(10);
+    assertThat(bean2.animal).isEqualTo("frog");
   }
 
+  static class SimpleBean {
+    public int count = 10;
+    public String animal = "frog";
+  }
 }
