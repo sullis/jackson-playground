90f724ca40123b0ab12e8417b822dd545e1db032
sullis <seans@grubhub.com>
2025-11-13 18:01:30 -0800

    tweak shell script

--- jackson-2-to-3.sh
+++ jackson-2-to-3.sh
@@ -1,4 +1,4 @@
 mvn -U org.openrewrite.maven:rewrite-maven-plugin:run \
     -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-jackson:RELEASE \
     -Drewrite.activeRecipes=org.openrewrite.java.jackson.UpgradeJackson_2_3 \
-    -Drewrite.exportDatatables=true
+    -Drewrite.exportDatatables=false
